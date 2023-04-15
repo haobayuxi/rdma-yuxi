ec827129bf561b6cd8fc1c832472691943297be1
haobayuxi <whaohit@gmail.com>
2023-04-15 23:03:26 +0800

    update

--- core/common/json.h
+++ core/common/json.h
@@ -1,13 +1,14 @@
 
 #include <json/json.h>
 
+#include <fstream>
 #include <iostream>
 #include <string>
 
 #include "common.h"
 
 SystemConfig read_config_from_file() {
-  //   std::string config_file = "system_config.json";
+  // std::string config_file = "system_config.json";
   Json::Reader reader;
   Json::Value root;
   ifstream srcFile("system_config.json", ios::binary);
